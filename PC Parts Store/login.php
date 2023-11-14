094ef06be3cf9076099a7eeef69b69af741c375b
Kenny Reyes <johnkennyreyes21@gmail.com>
2023-11-14 15:31:31 +0800

    Log In page functioning using php conditional

--- PC Parts Store/login.php
+++ PC Parts Store/login.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="styles.css">
   <title>PC Parts Store</title>
+  <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 <body>
 
@@ -14,20 +15,26 @@
 <form action="loginprocess.php" class="loginform" method="post" >
 
 <div class="container">
-<label class="label">Username: </label>
+<label class="label">Username </label>
 <input class="label" type="text" id="username" name="username" required>
 
 
 
-<label class="label">Password: </label>
-<input class="label" type="password" id="password" required>
-
+<label class="label">Password </label>
+<input class="label" type="password" id="password" name="password" required>
+<?php
+        // Display error message if redirected from process_form.php with invalid username
+        if(isset($_GET['error']) && $_GET['error'] === 'invalid_username') {
+            echo '<p class="error">Invalid username. Please try again.</p>';
+        }
+    ?>
 
 <input type="submit" value="Log In">
 </div>
 
 
 </form>
+
 </div>
 
 </body>
--- PC Parts Store/loginprocess.php
+++ PC Parts Store/loginprocess.php
@@ -4,17 +4,24 @@
         $username = $_POST['username'];
         $password = $_POST['password'];
         
+        // Correct username and password
+        $correct_username = "batdimoiprint";
+        $correct_password = "hatdog123";
+        
         // Validation logic
         if (empty($username) || empty($password)) {
             // Display error if fields are empty
             echo "Please fill in both username and password fields.";
+        } elseif ($username !== $correct_username || $password !== $correct_password) {
+            // Display error for incorrect username or password
+            header("Location: login.php?error=invalid_username&username=" . urlencode($username));
         } else {
-            // Validation successful, perform further actions (e.g., database operations, additional processing)
-            header("Location: index.html"); // Replace this with your desired action
+            // If both username and password are correct
+            header("Location: index.html"); // Redirect to successful login page
+            exit();
         }
     } else {
         // Redirect back to the form if accessed directly without form submission
-        
         exit();
     }
 ?>

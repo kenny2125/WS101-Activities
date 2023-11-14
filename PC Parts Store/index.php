afb8a4803feeea960ae38b74b3021f82b88e7dd5
Kenny Reyes <johnkennyreyes21@gmail.com>
2023-11-14 12:56:52 +0800

    Default State

--- PC Parts Store/index.php
+++ PC Parts Store/index.php
@@ -8,65 +8,110 @@
 </head>
 <body>
 
-  <div class="topnav">
+<div class="topnav">
     <a class="active" href="#home">Home</a>
     <a href="#about">About</a>
     <a href="#contact">Contact</a>
-    <a onclick="document.getElementById('id01').style.display='block'" href="#contact">Login</a>
+
 
     <div id="id01" class="modal">
   
-  <form class="modal-content animate" action="/action_page.php" method="post">
-    <div class="imgcontainer">
-      <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
-      <img src="Images/CCT logo.png" alt="Avatar" class="avatar">
-    </div>
-
-    <div class="container">
-      <label for="uname"><b>Username</b></label>
-      <input type="text" placeholder="Enter Username" name="uname" required>
-
-      <label for="psw"><b>Password</b></label>
-      <input type="password" placeholder="Enter Password" name="psw" required>
-        
-      <button type="submit">Login</button>
-      <label>
-        <input type="checkbox" checked="checked" name="remember"> Remember me
-      </label>
-    </div>
-
-    <div class="container" style="background-color:#f1f1f1">
-      <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
-      <span class="psw">Forgot <a href="#">password?</a></span>
-    </div>
-  </form>
-
   <script>
-// Get the modal
-var modal = document.getElementById('id01');
+    // Get the modal
+    var modal = document.getElementById('id01');
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
     }
-}
-</script>
+  </script>
 </div>
 
     <div class="search-container">
       <form action="/action_page.php">
-        <input type="text" placeholder="Search.." name="search">
-        <button type="submit">Submit</button>
+    
+        <input type="text" placeholder="Search.." id="mySearch"  onkeyup="myFunction()" placeholder="Search.." title="Type in a PC Part" name="search" >
+      
+        <ul id="myMenu">
+          <li><a href="#">CPU</a></li>
+          <li><a href="#">Cooler</a></li>
+          <li><a href="#">GPU</a></li>
+          <li><a href="#">RAM</a></li>
+          <li><a href="#">Motherboard</a></li>
+          <li><a href="#">PSU</a></li>
+          <li><a href="#">Chassis</a></li>
+          <li><a href="#">Accesories</a></li>
+  
+          <li><a href="#">Monitor</a></li>
+          
+        </ul>
+        
       </form>
     </div>
+    <script>
+      function myFunction() {
+        // Declare variables
+        var input, filter, ul, li, a, i;
+        input = document.getElementById("mySearch");
+        filter = input.value.toUpperCase();
+        ul = document.getElementById("myMenu");
+        li = ul.getElementsByTagName("li");
+
+        // Loop through all list items, and hide those who don't match the search query
+        for (i = 0; i < li.length; i++) {
+          a = li[i].getElementsByTagName("a")[0];
+          if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+          } else {
+            li[i].style.display = "none";
+          }
+        }
+      }
+    </script>
+</div>
+
+
+<div class="Welcome">
+ <p>Welcome to PC Parts Store!!</p>
+</div>
+
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="img_5terre.jpg">
+      <img src="Images/wgsegwegw.jpg" alt="Cinque Terre">
+    </a>
+    <div class="desc">Ryzen 9 5950x</div>
   </div>
-  
-  <div style="padding-left:16px">
-    <h2>Responsive Search Bar</h2>
-    <p>Navigation bar with a search box and a submit button inside of it.</p>
-    <p>Resize the browser window to see the responsive effect.</p>
+</div>
+
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="img_forest.jpg">
+      <img src="Images/31r2yodbPuL.jpg" alt="Forest">
+    </a>
+    <div class="desc">G.SKill  DDR4 64GB RAM</div>
   </div>
-  
+</div>
+
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="img_lights.jpg">
+      <img src="Images/713dOnHdPHL.jpg" alt="Northern Lights">
+    </a>
+    <div class="desc">MSI NVIDIA RTX 4090ti Gaming Trio</div>
+  </div>
+</div>
+
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="img_mountains.jpg">
+      <img src="Images/81jQ+FS5L3L.jpg" alt="Mountains">
+    </a>
+    <div class="desc">ASUS ROG Ryujin 240mm AIO Cooler</div>
+  </div>
+</div>
+
 </body>
 </html>
